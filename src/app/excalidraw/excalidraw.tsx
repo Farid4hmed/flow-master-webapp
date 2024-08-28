4c846b1b4ef2825b08f7b4bf52c66565bb5ec83c
Farid-SolaradAi <farid.ahmed@solarad.ai>
2024-08-29 01:11:03 +0530

    remove zen mode

--- src/app/excalidraw/excalidraw.tsx
+++ src/app/excalidraw/excalidraw.tsx
@@ -5,27 +5,32 @@ import mermaidToExcalidrawElements from './mermaidToExcali';
 
 export default function ExcalidrawWrapper(props: any) {
   const [elements, setElements] = useState([]);
-  console.log("chart\n", props.chart)
+  console.log("chart\n", elements)
   useEffect(() => {
-    const convertMermaidToElements = async () => {
-      const result: any = await mermaidToExcalidrawElements(props.chart); 
-      setElements(result); 
+    const convertMermaidToElements = async (code: any) => {
+      const result: any = await mermaidToExcalidrawElements(code);
+      setElements(result);
     };
 
-    convertMermaidToElements();
+    convertMermaidToElements(props.chart);
+
+    if (!elements) {
+      convertMermaidToElements(props.chart);
+    }
+
   }, [props.chart]);
 
-  if (props.isLoading) {
-    return <div className="w-full h-full bg-gray-100 flex justify-center items-center">
-      <div
-        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-success motion-reduce:animate-[spin_1.5s_linear_infinite]"
-        role="status">
-        <span
-          className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-        >Loading...</span>
-      </div>
-    </div>;
-  }
+  // if (props.isLoading) {
+  //   return <div className="w-full h-full bg-gray-100 flex justify-center items-center">
+  //     <div
+  //       className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-success motion-reduce:animate-[spin_1.5s_linear_infinite]"
+  //       role="status">
+  //       <span
+  //         className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+  //       >Loading...</span>
+  //     </div>
+  //   </div>;
+  // }
 
   return (
     <>
@@ -45,11 +50,11 @@ export default function ExcalidrawWrapper(props: any) {
             <div className="w-3/5 2xl:w-3/4 h-screen">
               <Excalidraw
                 initialData={{
-                  elements, 
-                  appState: { zenModeEnabled: true },
+                  elements,
+                  appState: { zenModeEnabled: false },
                   scrollToContent: true,
                 }}
-                key={props.chart} 
+                key={props.chart}
               />
             </div>
           </div>
